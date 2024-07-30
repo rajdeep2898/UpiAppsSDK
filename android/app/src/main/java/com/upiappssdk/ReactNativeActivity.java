be64692a2c58cb82146d6477f3c4a76a6525f868
Rajdeep Datta <rajdeepdatta@Rajdeeps-MacBook-Air.local>
2024-07-30 21:58:24 +0530

    add parent app and other fixes

---
+++ android/app/src/main/java/com/upiappssdk/ReactNativeActivity.java
@@ -0,0 +1,10 @@
+package com.upiappssdk;
+
+import com.facebook.react.ReactActivity;
+
+public class ReactNativeActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "UpiAppsSDK";
+    }
+}
